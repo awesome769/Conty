f788d9edf10c3e8d84d6e9e65df680ff44820aa8
awesome769 <776452@pdsb.net>
2025-02-10 03:04:28 +0000

    Update create-arch-bootstrap.sh

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -23,7 +23,7 @@ wine_pkgs="wine32"
 
 devel_pkgs="base-devel git meson mingw-w64-gcc cmake"
 
-gaming_pkgs="steam prismlauncher"
+gaming_pkgs="steam prismlauncher gamemode"
 
 extra_pkgs="nano ttf-dejavu ttf-liberation jre8-openjdk jre17-openjdk jre21-openjdk mangohud"
 
